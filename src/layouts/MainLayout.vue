48ec2e379d6f5c0c29e4110a3bea3c7cb12095e8
tancou <git.commits@tancou.me>
2024-02-07 07:44:37 +0100

    Add meta description

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -2,6 +2,7 @@
   <html lang="en">
     <head>
       <meta charset="utf-8" />
+      <meta name="description" content="Tancrède SUARD - Ingénieur DevOps - PHP/Laravel - Vue - Docker - Kubernetes - CI/CD - Virtualisation">
       <meta name="viewport" content="width=device-width" />
       <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
       <title>{{ title }}</title>
